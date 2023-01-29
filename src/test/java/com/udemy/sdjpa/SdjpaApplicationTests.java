f6c3d6dd9548b49855fd6c2b11b0c2b72a160a9f
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-01-29 18:27:03 +0100

    2.10 Use Spring Initializr to Create Project

---
+++ src/test/java/com/udemy/sdjpa/SdjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.sdjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
